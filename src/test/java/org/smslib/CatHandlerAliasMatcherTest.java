93aea2c9f192bcf75c2c9397df7335206203354c
Morgan Belkadi <morgan@frontlinesms.com>
2010-03-19 10:05:20 +0000

    Added the CatHandlerAliasMatcher test file to version control

---
+++ src/test/java/org/smslib/CatHandlerAliasMatcherTest.java
@@ -0,0 +1,30 @@
+package org.smslib;
+
+import junit.framework.TestCase;
+import net.frontlinesms.smsdevice.CatHandlerAliasMatcher;
+
+public class CatHandlerAliasMatcherTest extends TestCase {
+	/**
+	 * Test parsing of AT+CGMI responses in the {@link CService} class.
+	 */
+	public void testTranslateManufacturer() {
+		testTranslateManufacturer("Sony Ericsson", "SonyEricsson");
+		testTranslateManufacturer("nokia", "Nokia");
+		testTranslateManufacturer("MotorolaCECopyright2000", "Motorola");
+		testTranslateManufacturer("Lucky GoldStar", "LG");
+		testTranslateManufacturer("symBian", "Symbian");
+		testTranslateManufacturer("SamsungElectronicsCorporation", "Samsung");
+		testTranslateManufacturer("huawei", "Huawei");
+		testTranslateManufacturer("^RSSI huawei", "Huawei");
+	}
+	
+	/**
+	 * Test translating a manufacturer in the {@link CatHandlerAliasMatcher} class.
+	 * @param manufacturer The manufacturer getting translated.
+	 * @param expectedManufacturer The manufacturer string that we expect the CatHandlerAliasMatcher to return.
+	 */
+	private void testTranslateManufacturer(String manufacturer, String expectedManufacturer) {
+		String actualManufacturer = CatHandlerAliasMatcher.getInstance().translateManufacturer(manufacturer);
+		assertEquals(expectedManufacturer, actualManufacturer);
+	}
+}
