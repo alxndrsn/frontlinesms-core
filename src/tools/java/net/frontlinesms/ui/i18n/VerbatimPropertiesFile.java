0ffc4cafb981139a7ef94b5b15bd3d230d623def
Alex Anderson <alex@frontlinesms.com>
2010-05-17 12:01:47 +0100

    Updated VerbatimPropertiesFile.clone so that the original is not modified when a clone is modififed. Updated French and Russian translations with version generated by VerbatimPropertiesFile.

--- src/tools/java/net/frontlinesms/ui/i18n/VerbatimPropertiesFile.java
+++ src/tools/java/net/frontlinesms/ui/i18n/VerbatimPropertiesFile.java
@@ -13,6 +13,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -30,11 +32,7 @@ public class VerbatimPropertiesFile implements Cloneable {
 	}
 	
 	protected VerbatimPropertiesFile clone() {
-		try {
-			return (VerbatimPropertiesFile) super.clone();
-		} catch (CloneNotSupportedException ex) {
-			throw new IllegalStateException(ex);
-		}
+		return new VerbatimPropertiesFile(new ArrayList<String>(this.lines));
 	}
 
 	public Set<String> getKeys() {
