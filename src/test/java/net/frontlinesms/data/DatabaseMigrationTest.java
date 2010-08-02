4250c065ee4244ac8d25c73619fc4894f1782d97
Alex Anderson <alex@frontlinesms.com>
2010-08-02 12:41:30 +0100

    Wrote outline of MMS message db migration test.

---
+++ src/test/java/net/frontlinesms/data/DatabaseMigrationTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package net.frontlinesms.data;
+
+import net.frontlinesms.junit.BaseTestCase;
+
+/**
+ * @author Alex Anderson <alex@frontlinesms.com>
+ */
+public class DatabaseMigrationTest extends BaseTestCase {
+	public void testAddingMultimediaMessages() {
+		// TODO Initialise the database WITHOUT multimedia messages
+		// TODO persist some non-multimedia messages
+		// TODO deinitialise the database
+		// TODO Initiliase the database WITH multimedia messages
+		// TODO persist some multimedia messages
+		// TODO retrieve the multimedia messages
+		// TODO retrieve the non-multimedia messages
+	}
+}
