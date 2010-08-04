3c061d35f32fc63409b2e218327a72ef92f4e4cf
Morgan Belkadi <morgan@frontlinesms.com>
2010-08-04 13:45:17 +0100

    Updated snapshot dependencies Prepared for release 1.6.15-RC2

---
+++ src/tools/java/net/frontlinesms/encoding/EncodingTool.java
@@ -0,0 +1,33 @@
+package net.frontlinesms.encoding;
+
+/**
+ * @author Morgan Belkadi <morgan@frontlinesms.com>
+ *
+ */
+public class EncodingTool {
+//> STATIC CONSTANTS
+
+//> INSTANCE PROPERTIES
+
+//> CONSTRUCTORS
+
+//> INSTANCE METHODS
+
+//> ACCESSORS
+
+//> INSTANCE HELPER METHODS
+
+	public static void convertToUnicode(String[] sources) {
+		for (String source : sources) { 
+			for (char c : source.toCharArray()) {
+				System.err.println("'" + c + "' --> \\u" + Integer.toString(c, 16));
+			}
+		}
+	}
+//> STATIC FACTORIES
+
+//> STATIC HELPER METHODS
+	public static void main (String[] args) {
+		EncodingTool.convertToUnicode(args);
+	}
+}
