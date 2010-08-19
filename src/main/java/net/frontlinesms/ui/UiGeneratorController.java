f21d2f457572e92e51eab1898eabaf801e690601
Alex Anderson <alex@frontlinesms.com>
2010-08-19 13:13:39 +0100

    Removed unused import from UIGC

--- src/main/java/net/frontlinesms/ui/UiGeneratorController.java
+++ src/main/java/net/frontlinesms/ui/UiGeneratorController.java
@@ -30,9 +30,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.swing.SwingUtilities;
-
-
 import net.frontlinesms.*;
 import net.frontlinesms.data.*;
 import net.frontlinesms.data.domain.*;
