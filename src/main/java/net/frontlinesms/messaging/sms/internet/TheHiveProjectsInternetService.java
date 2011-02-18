c0dba91c3472d8762c0dc9dc0458267862e98ee2
Alex Anderson <alex@frontlinesms.com>
2011-02-18 15:06:37 +0000

    Added Skebby and Hive Projects internet services.

---
+++ src/main/java/net/frontlinesms/messaging/sms/internet/TheHiveProjectsInternetService.java
@@ -0,0 +1,7 @@
+package net.frontlinesms.messaging.sms.internet;
+
+import net.frontlinesms.messaging.Provider;
+
+@Provider(name="The Hive Projects", icon = "/icons/hive_projects_logo.png")
+public class TheHiveProjectsInternetService extends ClickatellInternetService {
+}
